ba9a2ba82d67b0d8b36f91700c00e77191590d34
Daniel Okumu <okumudaniel544@gmail.com>
2025-05-28 12:33:22 +0300

    geolocation API

--- starter/script.js
+++ starter/script.js
@@ -10,3 +10,20 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      console.log(position);
+      const { latitude } = position.coords; //refer to destructuring
+      const { longitude } = position.coords; //refer to destructuring
+      console.log(
+        `https://www.google.com/maps/@-${latitude},${longitude},2021m/data`
+      );
+    },
+    function () {
+      alert('Could not get your position');
+    }
+  ); //geolocation API : accepts two callback functions
+  // ...first one called on success and second one called on error
+}
